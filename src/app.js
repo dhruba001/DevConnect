21333890fbe5aa1e9cd4e9d6786ac98262040d38
Dhruba <gdhruba748+github@gmail.com>
2025-10-08 22:22:38 +0530

    error handling

--- src/app.js
+++ src/app.js
@@ -4,12 +4,23 @@ const app = express();
 
 app.use("/admin", adminAuth);
 
+app.get("/getUserData", (req, res) => {
+  throw new error("abcd");
+  res.send("new things");
+});
+
 app.get("/admin", (req, res, next) => {
   res.send("admin is validated so we're here");
 });
 
 app.use("/test", (req, res, next) => {
   res.send("first");
+});
+
+app.use("/", (err, req, res, next) => {
+  if (err) {
+    res.status(500).send("we encountered an error");
+  }
 });
 
 app.listen(7777, () => {
