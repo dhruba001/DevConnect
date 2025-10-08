f6b15ffdcedff8e77101ffc20181be4da112991e
Dhruba <gdhruba748+github@gmail.com>
2025-10-08 21:11:59 +0530

    multiple route handlers

--- src/app.js
+++ src/app.js
@@ -2,23 +2,34 @@ const express = require("express");
 
 const app = express(); // creating a new server using express
 
-app.get("/user/:userId/:name/:password", (req, res) => {
-  console.log(req.params); // to see what data is coming to us from client
-  res.send({ firstname: "Dhruba", lastname: "Goswami" });
-});
+app.use(
+  "/test",
+  (req, res, next) => {
+    res.send("first");
+    // next();
+  },
+  (req, res) => {
+    res.send("2nd");
+  }
+);
 
-app.post("/user", (req, res) => {
-  res.send("data sucessfully saved to db");
-});
+// app.get("/user/:userId/:name/:password", (req, res) => {
+//   console.log(req.params); // to see what data is coming to us from client
+//   res.send({ firstname: "Dhruba", lastname: "Goswami" });
+// });
 
-app.delete("/user", (req, res) => {
-  res.send("data sucessfully deleted");
-});
+// app.post("/user", (req, res) => {
+//   res.send("data sucessfully saved to db");
+// });
 
-app.use("/test", (req, res) => {
-  // this will match all like get post put etc
-  res.send("Hello from the server");
-}); // this function is known as request handler/ route handler
+// app.delete("/user", (req, res) => {
+//   res.send("data sucessfully deleted");
+// });
+
+// app.use("/test", (req, res) => {
+//   // this will match all like get post put etc
+//   res.send("Hello from the server");
+// }); // this function is known as request handler/ route handler
 
 app.listen(7777, () => {
   console.log("server is sucessfully running on port 7777");
