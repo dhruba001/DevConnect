25e2368ad6ed1736600b6aea3922ab47373c6fb3
Dhruba <gdhruba748+github@gmail.com>
2025-10-08 22:08:15 +0530

    calling middleware from seperate file

--- src/app.js
+++ src/app.js
@@ -1,36 +1,17 @@
 const express = require("express");
+const { adminAuth } = require("./middlewares/auth");
+const app = express();
 
-const app = express(); // creating a new server using express
+app.use("/admin", adminAuth);
 
-app.use(
-  "/test",
-  (req, res, next) => {
-    res.send("first");
-    // next();
-  },
-  (req, res) => {
-    res.send("2nd");
-  }
-);
+app.get("/admin", (req, res, next) => {
+  res.send("admin is validated so we're here");
+});
 
-// app.get("/user/:userId/:name/:password", (req, res) => {
-//   console.log(req.params); // to see what data is coming to us from client
-//   res.send({ firstname: "Dhruba", lastname: "Goswami" });
-// });
-
-// app.post("/user", (req, res) => {
-//   res.send("data sucessfully saved to db");
-// });
-
-// app.delete("/user", (req, res) => {
-//   res.send("data sucessfully deleted");
-// });
-
-// app.use("/test", (req, res) => {
-//   // this will match all like get post put etc
-//   res.send("Hello from the server");
-// }); // this function is known as request handler/ route handler
+app.use("/test", (req, res, next) => {
+  res.send("first");
+});
 
 app.listen(7777, () => {
   console.log("server is sucessfully running on port 7777");
-}); // now out server will first create a server and then listen for incoming traffic
+});
