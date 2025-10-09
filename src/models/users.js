a2f622cec14d7581f5b3b5b8024c25b9c0785d23
Dhruba <gdhruba748+github@gmail.com>
2025-10-09 20:49:26 +0530

    created user schema

---
+++ src/models/users.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: string,
+  },
+  lastname: {
+    type: string,
+  },
+  emailId: {
+    type: string,
+  },
+  password: {
+    type: string,
+  },
+  age: {
+    type: string,
+  },
+  gender: {
+    type: string,
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+module.exports = { User };
