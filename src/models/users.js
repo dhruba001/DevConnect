53f22b3801cc1440934f3f5909ed3cc3b4c8be37
Dhruba <gdhruba748+github@gmail.com>
2025-10-10 22:45:39 +0530

    db validation

--- src/models/users.js
+++ src/models/users.js
@@ -4,27 +4,39 @@ const userSchema = new mongoose.Schema({
   firstName: {
     type: String,
     required: true, // now it is required else it'll throw error if not provided
+    minLength: 4,
   },
   lastName: {
     type: String,
   },
   emailId: {
     type: String,
+    lowercase: true,
     required: true,
     unique: true, // email id will be unique, no two data point can have same email
+    trim: true,
   },
   password: {
     type: String,
     required: true,
   },
   age: {
-    type: String,
+    type: Number,
+    min: 18,
+    max: 50,
   },
   gender: {
     type: String,
+    validate(value) {
+      if (!["male", "female", "others"].includes(value)) {
+        throw new Error("Gender is not valid");
+      }
+    },
   },
   photoUrl: {
     type: String,
+    default:
+      "https://www.pngfind.com/pngs/m/610-6104451_image-placeholder-png-user-profile-placeholder-image-png.png",
   },
   about: {
     type: String,
