fd5b94a3679695750beb3f02f6941eb25f48e03d
Dhruba <gdhruba748+github@gmail.com>
2025-10-11 19:10:37 +0530

    new validator function for email in db level

--- src/models/users.js
+++ src/models/users.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 const userSchema = new mongoose.Schema(
   {
@@ -16,6 +17,11 @@ const userSchema = new mongoose.Schema(
       required: true,
       unique: true, // email id will be unique, no two data point can have same email
       trim: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("invalid email address" + value);
+        }
+      },
     },
     password: {
       type: String,
