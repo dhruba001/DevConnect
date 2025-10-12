76e95e417623ee9e476bdc45676dbbf097fe92bf
Dhruba <gdhruba748+github@gmail.com>
2025-10-12 17:06:41 +0530

    added login api

--- src/app.js
+++ src/app.js
@@ -29,6 +29,24 @@ app.post("/signup", async (req, res) => {
   }
 });
 
+app.post("/login", async (req, res) => {
+  try {
+    const { emailId, password } = req.body;
+    const user = await User.findOne({ emailId: emailId });
+    if (!user) {
+      throw new Error("EMail id is not present !! signup instead");
+    }
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (isPasswordValid) {
+      res.send("login successfull !!");
+    } else {
+      throw new Error("Password is incorrect !!");
+    }
+  } catch (err) {
+    res.status(400).send("error : " + err.message);
+  }
+});
+
 // Get user by email
 app.get("/user", async (req, res) => {
   const userEmail = req.body.emailId;
