bccffd129816f7849b02b3489ced04b9f6aa5b3e
Dhruba <gdhruba748+github@gmail.com>
2025-10-12 16:16:46 +0530

    formatting changes

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ app.post("/signup", async (req, res) => {
     await user.save();
     res.send("User added successfully");
   } catch (err) {
-    res.status(400).send("error :" + err.message);
+    res.status(400).send("error : " + err.message);
   }
 });
 
